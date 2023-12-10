21c4c9ceac4d3bbd4afc15d5f5d210bef4a1b5b6
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-10 15:33:24 +0000

    feat: add my favorite autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,4 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
+local function augroup(name)
+  return vim.api.nvim_create_augroup('P4P1' .. name, { clear = true })
+end
 
 -- dont list quickfix buffers
 autocmd("FileType", {
@@ -10,11 +13,29 @@ autocmd("FileType", {
 
 -- Highlight on yank
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = augroup('YankHighlight'),
   pattern = '*',
+})
+
+-- Close some filetypes with <q>
+autocmd("Filetype", {
+  group = augroup("close_with_q"),
+  -- NOTE:`:set ft?` to get the filetype of the current buffer
+  pattern = {
+    "PlenaryTestPopup",
+    "fugitive",
+    "help",
+    "lspinfo",
+    "man",
+    "checkhealth",
+    "qf"
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<CMD>close<CR>", { buffer = event.buffer, silent = true })
+  end
 })

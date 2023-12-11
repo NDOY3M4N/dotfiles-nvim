dc64b275f667e01b5d8cda91425337aee8466acb
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-12-11 01:02:56 +0000

    chore: remove boilerplate from kickstart

--- init.lua
+++ init.lua
@@ -21,18 +21,15 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Useful plugin to show you pending keybinds.
 -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
---       These are some example plugins that I've included in the kickstart repository.
---       Uncomment any of the lines below to enable them.
--- require 'kickstart.plugins.autoformat',
+-- These are some example plugins that I've included in the kickstart repository.
+-- Uncomment any of the lines below to enable them.
+--
 -- require 'kickstart.plugins.debug',
 
 -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
---    up-to-date with whatever is in the kickstart repo.
---    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 --
---    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
--- { import = 'custom.plugins' },
+-- You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
+-- up-to-date with whatever is in the kickstart repo.
 
 -- [[ Configure plugins ]]
 require('lazy').setup('plugins')

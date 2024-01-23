d660791675f0ef43f7fb4770ecd356a513ba235d
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-01-23 08:20:15 +0000

    refac: keymaps keymaps keymaps...

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -7,32 +7,22 @@ return {
     local harpoon = require('harpoon')
     harpoon:setup({})
 
-    vim.keymap.set('n', '<leader>ma', function()
-      harpoon:list():append()
-    end)
-    vim.keymap.set('n', '<leader>mm', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'Harpoon: ' .. desc
+      end
 
-    vim.keymap.set('n', '<A-h>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<A-j>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<A-k>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<A-l>', function()
-      harpoon:list():select(4)
-    end)
+      vim.keymap.set('n', keys, func, { desc = desc })
+    end
 
-    -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<A-p>', function()
-      harpoon:list():prev()
-    end)
-    vim.keymap.set('n', '<A-n>', function()
-      harpoon:list():next()
-    end)
+    nmap('<leader>ma', function() harpoon:list():append() end, "Append")
+    nmap('<leader>mm', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Toggle menu")
+
+    nmap('<A-p>', function() harpoon:list():prev() end, "select previous item")
+    nmap('<A-n>', function() harpoon:list():next() end, "select next item")
+    nmap('<A-h>', function() harpoon:list():select(1) end, "select item #1")
+    nmap('<A-j>', function() harpoon:list():select(2) end, "select item #2")
+    nmap('<A-k>', function() harpoon:list():select(3) end, "select item #3")
+    nmap('<A-l>', function() harpoon:list():select(4) end, "select item #4")
   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -68,9 +68,17 @@ return {
 
     vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
+    local function telescope_live_grep_open_files()
+      builtin.live_grep({
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+      })
+    end
+
     -- See `:help telescope.builtin`
-    vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+    -- vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>f/', telescope_live_grep_open_files, { desc = '[F]ind [/] in Open Files' })
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
@@ -79,23 +87,15 @@ return {
       }))
     end, { desc = '[/] Fuzzily search in current buffer' })
 
-    local function telescope_live_grep_open_files()
-      builtin.live_grep({
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      })
-    end
-
-    vim.keymap.set('n', '<leader>f/', telescope_live_grep_open_files, { desc = '[F]ind [/] in Open Files' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     -- vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+    -- vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [k]eymaps' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>fG', ':LiveGrepGitRoot<cr>', { desc = '[F]ind by [G]rep on Git Root' })
 
     vim.keymap.set('n', '<leader>th', function()

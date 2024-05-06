66c1626ea95a853b720319d0ac382425f525f36c
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2024-05-06 15:21:29 +0000

    chore: remove null-ls and use conform.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,113 +1,124 @@
 ---@type LazySpec
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'jay-babu/mason-null-ls.nvim',
-    'folke/which-key.nvim',
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'folke/which-key.nvim',
 
-    -- Useful status updates for LSP
-    { 'j-hui/fidget.nvim', opts = {} },
+      -- Useful status updates for LSP
+      { 'j-hui/fidget.nvim', opts = {} },
 
-    -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
-  },
-  config = function()
-    require('mason').setup()
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        'lua_ls',
-        'tsserver',
-        'svelte',
-        'eslint',
-        'gopls',
-      },
-    })
-    require('mason-null-ls').setup({
-      automatic_installation = false,
-      ensure_installed = {
-        'stylua',
-        'prettierd',
-        'gofumpt',
-        'golines',
-        'goimports-reviser',
-      },
-    })
-
-    local servers = {
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- html = { filetypes = { 'html', 'twig', 'hbs' } },
-
-      tsserver = {
-        javascript = {
-          inlayHints = {
-            includeInlayEnumMemberValueHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayParameterNameHints = 'all',
-            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayVariableTypeHints = true,
-          },
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+    config = function()
+      require('mason').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'lua_ls',
+          'tsserver',
+          'svelte',
+          'eslint',
+          'gopls',
         },
-        typescript = {
-          inlayHints = {
-            includeInlayEnumMemberValueHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayParameterNameHints = 'all',
-            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayVariableTypeHints = true,
-          },
-        },
-      },
-      lua_ls = {
-        Lua = {
-          telemetry = { enable = false },
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              [vim.fn.expand('$vimruntime/lua')] = true,
-              [vim.fn.expand('$vimruntime/lua/vim/lsp')] = true,
+      })
+
+      local servers = {
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- html = { filetypes = { 'html', 'twig', 'hbs' } },
+
+        tsserver = {
+          javascript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = true,
             },
           },
-          hint = { enable = true },
-          -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-          -- diagnostics = { disable = { 'missing-fields' } },
+          typescript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = 'all',
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = true,
+            },
+          },
         },
+        lua_ls = {
+          Lua = {
+            telemetry = { enable = false },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                [vim.fn.expand('$vimruntime/lua')] = true,
+                [vim.fn.expand('$vimruntime/lua/vim/lsp')] = true,
+              },
+            },
+            hint = { enable = true },
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
+          },
+        },
+        svelte = {},
+        gopls = {},
+        eslint = {},
+      }
+
+      -- Setup neovim lua configuration
+      require('neodev').setup()
+
+      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+      -- Ensure the servers above are installed
+      local mason_lspconfig = require('mason-lspconfig')
+
+      mason_lspconfig.setup({
+        ensure_installed = vim.tbl_keys(servers),
+      })
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          require('lspconfig')[server_name].setup({
+            capabilities = capabilities,
+            on_attach = require('utils').on_attach,
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes,
+          })
+        end,
+      })
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
       },
-      svelte = {},
-      gopls = {},
-      eslint = {},
-    }
-
-    -- Setup neovim lua configuration
-    require('neodev').setup()
-
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require('mason-lspconfig')
-
-    mason_lspconfig.setup({
-      ensure_installed = vim.tbl_keys(servers),
-    })
-
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        require('lspconfig')[server_name].setup({
-          capabilities = capabilities,
-          on_attach = require("utils").on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        })
-      end,
-    })
-  end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        go = { 'goimports-reviser', 'gofumpt', 'golines' },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        vue = { 'prettierd' },
+        svelte = { 'prettierd' },
+      },
+    },
+  },
 }
